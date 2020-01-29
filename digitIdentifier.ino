4433327206cae81cf9884b24f6f11c9fe3a31059
Aaron <mohammad.chegini@hotmail.com>
2020-01-29 18:48:54 +0330

    Digit Identifier

---
+++ digitIdentifier.ino
@@ -0,0 +1,34 @@
+void digitIdentifier(int number){
+  switch(number) {
+    case 1:
+        one();
+        break;
+    case 2:
+        two();
+        break;
+    case 3;
+        three();
+        break;
+    case 4;
+        four();
+        break;
+    case 5;
+        five();
+        break;
+    case 6:
+        six();
+        break;
+    case 7:
+        seven();
+        break;
+    case 8:
+        eight();
+        break;
+    case 9:
+        nine();
+        break;
+    case 0:
+        zero();
+        break;
+  }
+}
