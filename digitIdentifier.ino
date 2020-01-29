8f2e020397d57834c5a6096b0295bcd9d62173ef
Aaron <mohammad.chegini@hotmail.com>
2020-01-29 18:53:02 +0330

    Time Converter ongoing

--- digitIdentifier.ino
+++ digitIdentifier.ino
@@ -1,34 +1,67 @@
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) ; // wait for serial
+  delay(200);
+}
+
+void loop() {
+  tmElements_t tm;
+  if (RTC.read(tm)) {
+    printer(tm);
+  }
+  delay(1000);
+}
+
+void printer(tmElements_t _time){
+  digitIdentifier((_time.Hour/10)%10);
+  digitIdentifier(_time.Hour%10);
+}
+
 void digitIdentifier(int number){
   switch(number) {
     case 1:
-        one();
+        Serial.print('One');
+        Serial.println();
         break;
     case 2:
-        two();
+        Serial.print('Two');
+        Serial.println();
         break;
-    case 3;
-        three();
+    case 3:
+        Serial.print('Three');
+        Serial.println();
         break;
-    case 4;
-        four();
+    case 4:
+        Serial.print('Four');
+        Serial.println();
         break;
-    case 5;
-        five();
+    case 5:
+        Serial.print('Five');
+        Serial.println();
         break;
     case 6:
-        six();
+        Serial.print('Six');
+        Serial.println();
         break;
     case 7:
-        seven();
+        Serial.print('Seven');
+        Serial.println();
         break;
     case 8:
-        eight();
+        Serial.print('Eight');
+        Serial.println();
         break;
     case 9:
-        nine();
+        Serial.print('Nine');
+        Serial.println();
         break;
     case 0:
-        zero();
+        Serial.print('Zero');
+        Serial.println();
         break;
   }
 }
